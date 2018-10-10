874f08d0227d2528c62951a154acc81b993605a0
Jaaromy Zierse <jaaromy.zierse@gmail.com>
2018-10-10 16:00:51 -0600

    Add encode method.

--- src/buffer-utils.ts
+++ src/buffer-utils.ts
@@ -1,24 +1,29 @@
+declare const Buffer;
 const bufferProto = Buffer.prototype;
 
 /* tslint:disable:no-invalid-this */
 function writeIntLE(data: number, offset: number, size: number): number {
-    if (size === 1) {
-        return this.writeInt8(data, offset);
-    }
-    if (size === 2) {
-        return this.writeInt16LE(data, offset);
-    }
-    throw new Error('Size not supported in this polyfill. Buffer.writeIntLE:' + size);
+  if (size === 1) {
+    return this.writeInt8(data, offset);
+  }
+  if (size === 2) {
+    return this.writeInt16LE(data, offset);
+  }
+  throw new Error(
+    "Size not supported in this polyfill. Buffer.writeIntLE:" + size
+  );
 }
 
 function readIntLE(offset: number, size: number): number {
-    if (size === 1) {
-        return this.readInt8(offset);
-    }
-    if (size === 2) {
-        return this.readInt16LE(offset);
-    }
-    throw new Error('Size not supported in this polyfill, Buffer.readIntLE:' + size);
+  if (size === 1) {
+    return this.readInt8(offset);
+  }
+  if (size === 2) {
+    return this.readInt16LE(offset);
+  }
+  throw new Error(
+    "Size not supported in this polyfill, Buffer.readIntLE:" + size
+  );
 }
 /* tslint:enable:no-invalid-this */
 
@@ -31,59 +36,59 @@ bufferProto.readIntLE = bufferProto.readIntLE || readIntLE;
  * @constructor
  */
 export class BufferReader {
-    private buffer: Buffer;
-    private offset: number = 0;
+  private buffer: Buffer;
+  private offset: number = 0;
 
-    constructor(buffer: Buffer) {
-        this.buffer = buffer;
+  constructor(buffer: Buffer) {
+    this.buffer = buffer;
+  }
+
+  /**
+   * Read int in Big-endian order from internal buffer
+   * @param size
+   * @returns {*}
+   */
+  public readIntLE(size: number): number {
+    const data = this.buffer.readIntLE(this.offset, size);
+    this.offset += size;
+    return data;
+  }
+
+  public readMultibyteNumLE(size: number): number {
+    let num: number;
+    switch (size) {
+      case 2:
+        num = this.buffer.readUInt16LE(this.offset);
+        break;
+      case 4:
+        num = this.buffer.readUInt32LE(this.offset);
+        break;
+      default:
+        throw new Error(size + " is not supported as multibyte number");
     }
+    this.offset += size;
+    return num;
+  }
 
-    /**
-     * Read int in Big-endian order from internal buffer
-     * @param size
-     * @returns {*}
-     */
-    public readIntLE(size: number): number {
-        const data = this.buffer.readIntLE(this.offset, size);
-        this.offset += size;
-        return data;
-    };
+  /**
+   * Read size of data to a buffer from internal buffer
+   * @param size
+   * @returns {*}
+   */
+  public readBuffer(size: number): Buffer {
+    const data = this.buffer.slice(this.offset, this.offset + size);
+    this.offset += size;
+    return data;
+  }
 
-    public readMultibyteNumLE(size: number): number {
-        let num: number;
-        switch (size) {
-            case 2:
-                num = this.buffer.readUInt16LE(this.offset);
-                break;
-            case 4:
-                num = this.buffer.readUInt32LE(this.offset);
-                break;
-            default:
-                throw new Error(size + ' is not supported as multibyte number');
-        }
-        this.offset += size;
-        return num;
-    };
-
-    /**
-     * Read size of data to a buffer from internal buffer
-     * @param size
-     * @returns {*}
-     */
-    public readBuffer(size: number): Buffer {
-        const data = this.buffer.slice(this.offset, this.offset + size);
-        this.offset += size;
-        return data;
-    };
-
-    /**
-     * Read size of string from buffer
-     * @param size
-     * @returns {*|string}
-     */
-    public readString(size: number): string {
-        return this.readBuffer(size).toString('utf8');
-    };
+  /**
+   * Read size of string from buffer
+   * @param size
+   * @returns {*|string}
+   */
+  public readString(size: number): string {
+    return this.readBuffer(size).toString("utf8");
+  }
 }
 
 /**
@@ -92,63 +97,69 @@ export class BufferReader {
  * @constructor
  */
 export class BufferWriter {
-    private buffer: Buffer;
-    private offset: number = 0;
+  private buffer: Buffer;
+  private offset: number = 0;
 
-    constructor(buffer: Buffer) {
-        this.buffer = buffer;
-    };
+  constructor(buffer: Buffer) {
+    this.buffer = buffer;
+  }
 
-    /**
-     * Write  int to buffer in Big-endian order
-     * @param data
-     * @param size
-     */
-    public writeIntLE(data: number, size: number) {
-        this.buffer.writeIntLE(data, this.offset, size);
-        this.offset += size;
-    };
+  /**
+   * Write  int to buffer in Big-endian order
+   * @param data
+   * @param size
+   */
+  public writeIntLE(data: number, size: number) {
+    this.buffer.writeIntLE(data, this.offset, size);
+    this.offset += size;
+  }
 
-    public writeMultibyteNumLE(data: number, size: number) {
-        switch (size) {
-            case 2:
-                this.buffer.writeUInt16LE(data, this.offset);
-                break;
-            case 4:
-                this.buffer.writeUInt32LE(data, this.offset);
-                break;
-            default:
-                throw new Error(size + ' is not supported as multibyte number');
-        }
-        this.offset += size;
-    };
+  public writeMultibyteNumLE(data: number, size: number) {
+    switch (size) {
+      case 2:
+        this.buffer.writeUInt16LE(data, this.offset);
+        break;
+      case 4:
+        this.buffer.writeUInt32LE(data, this.offset);
+        break;
+      default:
+        throw new Error(size + " is not supported as multibyte number");
+    }
+    this.offset += size;
+  }
 
-    /**
-     * Write string in utf8 format to the buffer
-     * @param data
-     * @param size
-     */
-    public writeString(data: string, size: number) {
-        this.buffer.write(data, this.offset, size, 'utf8');
-        this.offset += size;
-    };
+  /**
+   * Write string in utf8 format to the buffer
+   * @param data
+   * @param size
+   */
+  public writeString(data: string, size: number) {
+    this.buffer.write(data, this.offset, size, "utf8");
+    this.offset += size;
+  }
 
-    /**
-     * Copy size of data from srcBuffer to the internal buffer
-     * @param srcBuffer
-     * @param size
-     */
-    public copyFrom(srcBuffer: Buffer, size: number) {
-        srcBuffer.copy(this.buffer, this.offset);
-        this.offset += size;
-    };
+  public write(data: any[]) {
+    for (let i = 0; i < data.length; i++) {
+      const element = data[i];
+      this.writeIntLE(element, 2);
+    }
+  }
 
-    /**
-     * Get back internal buffer
-     * @returns {*}
-     */
-    public getBuffer(): Buffer {
-        return this.buffer;
-    };
+  /**
+   * Copy size of data from srcBuffer to the internal buffer
+   * @param srcBuffer
+   * @param size
+   */
+  public copyFrom(srcBuffer: Buffer, size: number) {
+    srcBuffer.copy(this.buffer, this.offset);
+    this.offset += size;
+  }
 
+  /**
+   * Get back internal buffer
+   * @returns {*}
+   */
+  public getBuffer(): Buffer {
+    return this.buffer;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,139 @@
 /// <reference path="../typings/index.d.ts"/>
 
-import {BufferReader} from  './buffer-utils';
-import {FIREHOSE_ENVELOPE} from './envelope-structure';
-import * as uuid from 'node-uuid';
-import {KinesisBridgeEnvelope} from './kinesis-bridge-envelope';
+import { BufferReader, BufferWriter } from "./buffer-utils";
+import { FIREHOSE_ENVELOPE } from "./envelope-structure";
+import * as uuid from "node-uuid";
+import { KinesisBridgeEnvelope } from "./kinesis-bridge-envelope";
 
 export class KinesisBridgeEnvelopeParser {
-
-    /**
-     * Parses a base64 encoded Kinesis Bridge envelope string payload
-     * @param base64Data
-     * @returns {*}
-     */
-    public parseData(base64Data: string): KinesisBridgeEnvelope {
-        const reader = new BufferReader(new Buffer(base64Data, 'base64'));
-        const envelope = new KinesisBridgeEnvelope();
-        envelope.headerVersion = reader.readIntLE(FIREHOSE_ENVELOPE.HEADER_VERSION);
-        envelope.timeUUID = uuid.unparse(reader.readBuffer(FIREHOSE_ENVELOPE.TIME_UUID));
-        envelope.sourceNameLength = reader.readMultibyteNumLE(FIREHOSE_ENVELOPE.SOURCE_NAME_LENGTH);
-        envelope.sourceName = reader.readString(envelope.sourceNameLength);
-        envelope.sourcePropertiesLength = reader.readMultibyteNumLE(FIREHOSE_ENVELOPE.SOURCE_PROPERTIES_LENGTH);
-        const serializedSourceProperties = reader.readString(envelope.sourcePropertiesLength);
-        if (serializedSourceProperties.length > 0) {
-            envelope.sourceProperties = JSON.parse(serializedSourceProperties);
-        } else {
-            envelope.sourceProperties = {};
-        }
-
-        envelope.targetNameLength = reader.readMultibyteNumLE(FIREHOSE_ENVELOPE.TARGET_NAME_LENGTH);
-        envelope.targetName = reader.readString(envelope.targetNameLength);
-        envelope.targetPropertiesLength = reader.readMultibyteNumLE(FIREHOSE_ENVELOPE.TARGET_PROPERTIES_LENGTH);
-        envelope.targetProperties = JSON.parse(reader.readString(envelope.targetPropertiesLength));
-        envelope.contentLength = reader.readMultibyteNumLE(FIREHOSE_ENVELOPE.CONTENT_LENGTH);
-        envelope.contentBody = reader.readBuffer(envelope.contentLength);
-        return envelope;
+  /**
+   * Parses a base64 encoded Kinesis Bridge envelope string payload
+   * @param base64Data
+   * @returns {*}
+   */
+  public parseData(base64Data: string): KinesisBridgeEnvelope {
+    const reader = new BufferReader(new Buffer(base64Data, "base64"));
+    const envelope = new KinesisBridgeEnvelope();
+    envelope.headerVersion = reader.readIntLE(FIREHOSE_ENVELOPE.HEADER_VERSION);
+    envelope.timeUUID = uuid.unparse(
+      reader.readBuffer(FIREHOSE_ENVELOPE.TIME_UUID)
+    );
+    envelope.sourceNameLength = reader.readMultibyteNumLE(
+      FIREHOSE_ENVELOPE.SOURCE_NAME_LENGTH
+    );
+    envelope.sourceName = reader.readString(envelope.sourceNameLength);
+    envelope.sourcePropertiesLength = reader.readMultibyteNumLE(
+      FIREHOSE_ENVELOPE.SOURCE_PROPERTIES_LENGTH
+    );
+    const serializedSourceProperties = reader.readString(
+      envelope.sourcePropertiesLength
+    );
+    if (serializedSourceProperties.length > 0) {
+      envelope.sourceProperties = JSON.parse(serializedSourceProperties);
+    } else {
+      envelope.sourceProperties = {};
     }
+
+    envelope.targetNameLength = reader.readMultibyteNumLE(
+      FIREHOSE_ENVELOPE.TARGET_NAME_LENGTH
+    );
+    envelope.targetName = reader.readString(envelope.targetNameLength);
+    envelope.targetPropertiesLength = reader.readMultibyteNumLE(
+      FIREHOSE_ENVELOPE.TARGET_PROPERTIES_LENGTH
+    );
+    envelope.targetProperties = JSON.parse(
+      reader.readString(envelope.targetPropertiesLength)
+    );
+    envelope.contentLength = reader.readMultibyteNumLE(
+      FIREHOSE_ENVELOPE.CONTENT_LENGTH
+    );
+    envelope.contentBody = reader.readBuffer(envelope.contentLength);
+    return envelope;
+  }
+
+  public encode(envelope: KinesisBridgeEnvelope): Buffer {
+    let bodyString;
+
+    if (typeof envelope.contentBody === "string") {
+      envelope.contentBody = JSON.parse(envelope.contentBody);
+    }
+
+    bodyString = JSON.stringify(envelope.contentBody);
+
+    envelope.contentLength = Buffer.byteLength(bodyString, "utf8");
+    if (!envelope.sourceProperties) {
+      envelope.sourceProperties = {};
+    }
+    let sourcePropertiesString = JSON.stringify(envelope.sourceProperties);
+    envelope.sourcePropertiesLength = Buffer.byteLength(
+      sourcePropertiesString,
+      "utf8"
+    );
+    let timeString = Buffer.from(uuid.parse(envelope.timeUUID)).toString(
+      "utf8"
+    );
+    let targetPropertiesString = JSON.stringify(envelope.targetProperties);
+    envelope.targetPropertiesLength = Buffer.byteLength(
+      targetPropertiesString,
+      "utf8"
+    );
+
+    let bufsize =
+      FIREHOSE_ENVELOPE.HEADER_VERSION +
+      FIREHOSE_ENVELOPE.TIME_UUID +
+      FIREHOSE_ENVELOPE.SOURCE_NAME_LENGTH +
+      FIREHOSE_ENVELOPE.SOURCE_PROPERTIES_LENGTH +
+      FIREHOSE_ENVELOPE.TARGET_NAME_LENGTH +
+      FIREHOSE_ENVELOPE.TARGET_PROPERTIES_LENGTH +
+      FIREHOSE_ENVELOPE.CONTENT_LENGTH +
+      envelope.contentLength +
+      Buffer.byteLength(envelope.sourceName, "utf8") +
+      envelope.sourcePropertiesLength +
+      Buffer.byteLength(envelope.targetName, "utf8") +
+      envelope.targetPropertiesLength;
+
+    const writer = new BufferWriter(Buffer.alloc(bufsize));
+    writer.writeIntLE(envelope.headerVersion, FIREHOSE_ENVELOPE.HEADER_VERSION);
+    writer.writeString(timeString, FIREHOSE_ENVELOPE.TIME_UUID);
+    writer.writeMultibyteNumLE(
+      Buffer.byteLength(envelope.sourceName, "utf8"),
+      FIREHOSE_ENVELOPE.SOURCE_NAME_LENGTH
+    );
+    writer.writeString(
+      envelope.sourceName,
+      Buffer.byteLength(envelope.sourceName, "utf8")
+    );
+    writer.writeMultibyteNumLE(
+      envelope.sourcePropertiesLength,
+      FIREHOSE_ENVELOPE.SOURCE_PROPERTIES_LENGTH
+    );
+
+    writer.writeString(sourcePropertiesString, envelope.sourcePropertiesLength);
+
+    writer.writeMultibyteNumLE(
+      Buffer.byteLength(envelope.targetName, "utf8"),
+      FIREHOSE_ENVELOPE.TARGET_NAME_LENGTH
+    );
+
+    writer.writeString(
+      envelope.targetName,
+      Buffer.byteLength(envelope.targetName, "utf8")
+    );
+
+    writer.writeMultibyteNumLE(
+      envelope.targetPropertiesLength,
+      FIREHOSE_ENVELOPE.TARGET_PROPERTIES_LENGTH
+    );
+    writer.writeString(targetPropertiesString, envelope.targetPropertiesLength);
+
+    writer.writeMultibyteNumLE(
+      envelope.contentLength,
+      FIREHOSE_ENVELOPE.CONTENT_LENGTH
+    );
+
+    writer.writeString(bodyString, envelope.contentLength);
+
+    let ret = writer.getBuffer();
+    return ret;
+  }
 }
